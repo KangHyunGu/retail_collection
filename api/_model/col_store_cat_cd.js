f5093bcd0f68da48004be2cf04c2e29ce328ef81
DESKTOP-T39A46V\user <gusrn0380@gmail.com>
2024-09-23 17:12:19 +0900

    col_store_cat_cd 구조변경

--- api/_model/col_store_cat_cd.js
+++ api/_model/col_store_cat_cd.js
@@ -3,7 +3,13 @@ const TABLE = require('../../utils/TABLE');
 
 const catStoreCatCd = {
     async getAllCatCodes(){
-        const sql = `SELECT * FROM ${TABLE.COL_STORE_CAT_CD}`;
+        const sql = `SELECT CAT_CD.CAT_ID, 
+                            CAT_CD.CAT_CD, 
+                            CAT_CD.CAT_NM,  
+                            (SELECT MAIN_CAT_NM FROM col_store_main_cat_cd
+                                WHERE MAIN_CAT_ID = CAT_CD.MAIN_CAT_ID
+                            ) AS MAIN_CAT_NM
+                            FROM ${TABLE.COL_STORE_CAT_CD} CAT_CD`;
         const [row] = await db.execute(sql);
         return row;
     },
